dc20389ab71ba48a5076c5c4e7ccf7ef01fd124a
tabspear <tabspear@github.com>
2022-06-09 22:09:13 -0700

    Feat(Client):Create Navbar Background Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,47 @@
 module.exports = {
   content: [
-    "./layouts/*.html"
+    "./layouts/*.html",
+    "./layouts/*.js"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'black' : "#333",
+      'red': '#FF0000',
+    },
+
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [
