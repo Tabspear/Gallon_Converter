19e0abc8a5cc01c82e74df42371e908aca232f68
tabspear <tabspear@github.com>
2022-06-20 10:28:58 -0700

    ready to deploy

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const app = express()
 app.use(express.static("layouts"))
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3003, 
